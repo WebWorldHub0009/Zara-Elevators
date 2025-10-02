d6c8df5eccbc56338b477798e618bfdfc93b2230
projectswebworldhub009 <projectswebworldhub009@gmail.com>
2025-10-02 22:08:35 +0530

    image error import

--- src/component/ServiceComponent/ServicesSection.jsx
+++ src/component/ServiceComponent/ServicesSection.jsx
@@ -1,61 +1,73 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import sc1logo from "../../assets/images/services/sc1/sc1logo.jpeg";
+import sc2logo from "../../assets/images/services/sc2/sc2logo.jpeg";
+import sc3logo from "../../assets/images/services/sc3/sc3logo.jpeg";
+import sc4logo from "../../assets/images/services/sc4/sc4logo.jpeg";
+import sc5logo from "../../assets/images/services/sc5/sc5logo.jpeg";
+import sc6logo from "../../assets/images/services/sc6/sc6logo.jpeg";
+import sc7logo from "../../assets/images/services/sc7/sc7logo.jpeg";
+import sc8logo from "../../assets/images/services/sc8/sc8logo.jpeg";
+import sc9logo from "../../assets/images/services/sc9/sc9logo.jpeg";
+
 const services = [
   {
     id: 1,
-    title: "Traction Machine Room Elevator ",
+    title: "Traction Machine Room Elevator",
     desc: "Traction Machine Room Elevators are advanced vertical transportation systems...",
-    img: "src/assets/images/services/sc1/sc1logo.jpeg",
+    img: sc1logo,
   },
   {
     id: 2,
     title: "Home Elevator",
-    desc: "Home Elevators are designed to bring convenience, safety,and...",
-    img: "src/assets/images/services/sc2/sc2logo.jpeg",
+    desc: "Home Elevators are designed to bring convenience, safety, and...",
+    img: sc2logo,
   },
   {
     id: 3,
     title: "MRL Gealess Elevator",
     desc: "MRL (Machine Room-Less) Gealess Elevators are advanced vertical transportation...",
-    img: "src/assets/images/services/sc3/sc3logo.jpeg",
+    img: sc3logo,
   },
   {
     id: 4,
     title: "Hospital Elevator",
     desc: "Hospital Elevators are specialized vertical transportation systems...",
-    img: "src/assets/images/services/sc4/sc4logo.jpeg",
+    img: sc4logo,
   },
   {
     id: 5,
     title: "Goods Elevator",
     desc: "Goods Elevators are robust vertical transport systems designed specifically...",
-    img: "src/assets/images/services/sc5/sc5logo.jpeg",
+    img: sc5logo,
   },
   {
     id: 6,
     title: "Copasule Elevator",
-    desc: "QCopasule Elevators are compact, efficient vertical transport solutions...",
-    img: "src/assets/images/services/sc6/sc6logo.jpeg",
+    desc: "Copasule Elevators are compact, efficient vertical transport solutions...",
+    img: sc6logo,
   },
   {
     id: 7,
     title: "Parking Elevator",
     desc: "Parking Elevators are innovative vertical transport systems...",
-    img: "src/assets/images/services/sc7/sc7logo.jpeg",
+    img: sc7logo,
   },
   {
     id: 8,
     title: "Hydroulic Elevator",
     desc: "Hydraulic Elevators are advanced vertical transport systems...",
-    img: "src/assets/images/services/sc8/sc8logo.jpeg",
+    img: sc8logo,
   },
   {
     id: 9,
     title: "Parts And Machines",
     desc: "Elevator Parts and Machines comprise the essential components...",
-    img: "src/assets/images/services/sc9/sc9logo.jpeg",
+    img: sc9logo,
   },
+
+
 ];
 
 export default function ServicesSection() {
